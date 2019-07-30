511e0292393503a9180d13994a92fb47383bec7f
nanda <nandaadisaputra18@gmail.com>
2019-07-30 12:17:54 +0700

    crud sqlite kotlin

---
+++ app/src/main/java/com/nandaadisaputra/crud_sqlite_kotlin_biodata/StudentContract.kt
@@ -0,0 +1,25 @@
+package com.nandaadisaputra.crud_sqlite_kotlin_biodata
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+//TODO 7 Buat sebuah kelas model
+
+@Parcelize
+data class StudentContract(
+    val id: Long?,
+    val name: String,
+    val age: Int,
+    val address: String,
+    val photo: String?,
+    val majority: String
+) : Parcelable {
+    companion object{
+        const val TABLE_STUDENT = "table_student"
+        const val ID = "id"
+        const val NAME = "name"
+        const val AGE = "age"
+        const val ADDRESS = "address"
+        const val PHOTO = "photo"
+        const val MAJORITY = "majority"
+    }
+}
